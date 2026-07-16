484088c44f6f8e77026cca66df270a3116ba709c
Muhammad faisal <muhammadfaisal7864309@gmail.com>
2026-07-16 14:04:09 +0500

    making folders and also setup of gateway and authentication server and database setup

---
+++ AgentMind/backend/services/AuthService/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+dotenv.config();
+connectDB();
+
+const port = process.env.PORT || 8001;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("AuthService is running");
+});
+
+app.listen(port, () => {
+  console.log(`AuthService is running on port ${port}`);
+  
+});
